24882f76dc6797cb24928f518c7152bd113e9d64
sacurio <sandy.acurio@gmail.com>
2019-11-25 11:34:34 -0500

    Print hidden service url.

--- setup-nextcloud.sh
+++ setup-nextcloud.sh
@@ -8,6 +8,10 @@ ONION_URL=$(sudo cat $HSDIR_ROOT/nextcloud/hostname)
 TBB_EXIST=$(which tor-browser)
 if [ x$TBB_EXIST == "x" ]; then
     torbrowser-launcher http://$ONION_URL:81 &
-else 
+else    
+    printf "\n\n=====================\n"
+    printf "    http://$ONION_URL:81\n\n"
+    printf "=====================\n\n"
     tor-browser http://$ONION_URL:81 &
+    firefox http://127.0.0.1.81 &
 fi
