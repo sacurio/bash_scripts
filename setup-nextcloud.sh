045d4c75473ce91fee06f6f82a6ce7dbf6e071bc
sacurio <sandy.acurio@gmail.com>
2019-11-26 11:03:55 -0500

    Code improvements.

--- setup-nextcloud.sh
+++ setup-nextcloud.sh
@@ -10,15 +10,15 @@ sudo ./setup-nextcloud-with-permissions.sh
 
 #Retrieve the Hidden Service address configured.
 ONION_URL=$(sudo cat $HSDIR_ROOT/nextcloud/hostname)
-HS_URL=""
+HS_URL="http://${ONION_URL}:81"    
+green_msg "\n\nCopy and paste the next address in your Tor browser:\n"
+green_msg "$HS_URL\n\n"
+echo ${HS_URL} | xclip -selection c
+green_msg "The Onion Hidden Service address was copied to your clipboard.\n\n"
+
 FF_EXIST=$(which firefox)
 if [ x$FF_EXIST == "x" ]; then
     red_msg "\n\nFirefox is not installed.\n"
-else    
-    HS_URL="http://${ONION_URL}:81"    
-    green_msg "\n\nCopy and paste the next address in your Tor browser:\n"
-    green_msg "$HS_URL\n\n"
-    echo ${HS_URL} | xclip -selection c
-    green_msg "The Onion Hidden Service address was copied to your clipboard.\n\n"
+else        
     firefox http://127.0.0.1:81 &
 fi
